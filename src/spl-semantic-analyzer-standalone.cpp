c0763f95de0fdaca64d0624074564246620160d9
Trust_04zh <trust04zh@gmail.com>
2022-11-24 22:25:24 +0800

    add test cases

--- src/spl-semantic-analyzer-standalone.cpp
+++ src/spl-semantic-analyzer-standalone.cpp
@@ -1,6 +1,6 @@
 // #define SPL_SEMANTIC_ANALYZER_VERBOSE
 // uncomment this line to enable verbose output in semantic analyzer
-// #define LOCAL_SCOPE  // uncomment this line to enable local scope
+// #define LOCAL_SCOPE // uncomment this line to enable local scope
 #include "spl-ast.hpp"
 #include "spl-semantic-analyzer-body.cpp"
 #include "spl-semantic-error.hpp"
